8c4a910871c4b936a88c40349567a719202e46f1
Vahide Segah Üney <vahidesegah@gmail.com>
2025-05-15 16:53:48 +0300

    middleware

--- src/server.js
+++ src/server.js
@@ -22,12 +22,24 @@ export function setupServer() {
     }),
   );
 
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: 'Something went wrong',
     });
   });
-
 
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
