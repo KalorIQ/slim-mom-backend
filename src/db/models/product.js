9a0ecf2d6e7a1535b1e3ec41b816ebd4e15de037
Vahide Segah Üney <vahidesegah@gmail.com>
2025-05-16 20:37:29 +0300

    şemalar

--- src/db/models/product.js
+++ src/db/models/product.js
@@ -1,26 +1,36 @@
-import { Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const productsSchema = new mongoose.Schema(
+const productSchema = new Schema(
   {
     categories: {
       type: String,
+      required: true,
     },
     weight: {
       type: Number,
+      required: true,
     },
     title: {
       type: String,
+      required: true,
     },
     calories: {
       type: Number,
-    },
-    groupBloodNotAllowed: {
-      type: [Boolean],
       required: true,
+    },
+    groupBloodTypeNotAllowed: {
+      1: { type: Boolean, required: true },
+      2: { type: Boolean, required: true },
+      3: { type: Boolean, required: true },
+      4: { type: Boolean, required: true },
     },
   },
   {
+    timestamps: false,
     versionKey: false,
-    timestamps: true,
   },
 );
+
+const Product = model('product', productSchema);
+
+export default Product;
