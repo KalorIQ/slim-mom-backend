f60af3d75f3b9c3adaffcbdacc075f88a2cb24f0
Vahide Segah Üney <vahidesegah@gmail.com>
2025-05-19 21:26:42 +0300

    sss

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,5 @@
-import Product from "../db/models/products.js";
-import { errorHandler } from "../middlewares/errorHandler.js";
+import Product from '../db/models/products.js';
+import { errorHandler } from '../middlewares/errorHandler.js';
 
 export async function getProductsByQuery(req, res, next) {
   try {
@@ -8,12 +8,71 @@ export async function getProductsByQuery(req, res, next) {
       return next(errorHandler);
     }
 
-    const regex = new RegExp(title, "i");
+    const regex = new RegExp(title, 'i');
     const data = await Product.find({ title: regex }).limit(10);
 
     res.status(200).json({
-      message: "Products matching your search",
+      message: 'Products matching your search',
       data: data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getAllProducts(req, res, next) {
+  try {
+    const allProducts = await Product.find();
+    const products = allProducts.map((el) => {
+      return {
+        kcal: el.kcal,
+        weight: el.weight,
+        title: el.title,
+        id: el._id,
+      };
+    });
+    res.status(200).json({
+      message: 'All products',
+      data: products,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function addProduct(req, res, next) {
+  try {
+    const { title, weight, kcal } = req.body;
+    if (!title || !weight || !kcal) {
+      return next(errorHandler);
+    }
+
+    const newProduct = await Product.create({ title, weight, kcal });
+    res.status(201).json({
+      message: 'Product added successfully',
+      data: newProduct,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+export async function removeProduct(req, res, next) {
+  try {
+    const { productId } = req.params;
+    if (!productId) {
+      return next(errorHandler);
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+    if (!deletedProduct) {
+      return res.status(404).json({
+        message: 'Product not found',
+      });
+    }
+
+    res.status(200).json({
+      message: 'Product removed successfully',
+      data: deletedProduct,
     });
   } catch (error) {
     next(error);
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,9 +1,17 @@
-import { Router } from "express";
-import ctrlWrapper from "../utils/ctrlWrapper.js";
-import { getProductsByQuery } from "../controllers/products.js";
+import { Router } from 'express';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import {
+  getProductsByQuery,
+  getAllProducts,
+  addProduct,
+  removeProduct,
+} from '../controllers/products.js';
 
 const router = Router();
 
-router.get("/searchProducts", ctrlWrapper(getProductsByQuery));
+router.get('/searchProducts', ctrlWrapper(getProductsByQuery));
+router.get('/allProducts', ctrlWrapper(getAllProducts));
+router.post('/addProduct', ctrlWrapper(addProduct));
+router.delete('/removeProduct', ctrlWrapper(removeProduct));
 
 export default router;
